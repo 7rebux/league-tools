c8ab042952bfe9da7ee7ad6300b139a20723221d
Nils <58339645+7rebux@users.noreply.github.com>
2022-02-08 10:02:51 +0100

    :bug: Fixed props bug

--- src/renderer/components/ProfileBadge.js
+++ src/renderer/components/ProfileBadge.js
@@ -1,16 +1,24 @@
+const { Component } = require('react')
+
 import './ProfileBadge.sass'
 
-function ProfileBadge() {
-  return (
-    <div className={'badge ' + this.props.color}>
-      <div className='outer'>
-        <div className='inner'>
-          <img className='icon' src={this.props.icon} />
-          <span className='text'>{this.props.text}</span>
+class ProfileBadge extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className={'badge ' + this.props.color}>
+        <div className='outer'>
+          <div className='inner'>
+            <img className='icon' src={this.props.icon} />
+            <span className='text'>{this.props.text}</span>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default ProfileBadge

7c8f849b8040741be7b6cbe500924a60295bc892
Nils <58339645+7rebux@users.noreply.github.com>
2022-02-10 11:18:03 +0100

    :construction: Add temporary navigation buttons

--- src/renderer/pages/Home.js
+++ src/renderer/pages/Home.js
@@ -1,11 +1,16 @@
+const { useNavigate } = require('react-router-dom')
+
 import Profile from '../components/Profile'
 
 import './Home.sass'
 
 function Home() {
+  const navigate = useNavigate()
+
   return (
     <div className='page home'>
       <Profile />
+      <button onClick={() => navigate('../availability')}>Availability</button>
     </div>
   )
 }

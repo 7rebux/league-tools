931736001dd5af5c6e277ddb73688dc76f8bf120
Nils <58339645+7rebux@users.noreply.github.com>
2022-02-11 08:08:46 +0100

    :art: Now shows the current version

--- src/renderer/pages/Login.js
+++ src/renderer/pages/Login.js
@@ -10,13 +10,14 @@ function Login() {
     <div className='page login'>
       <div>
         <h1>
-          Welcome to <span>League Tools</span>
+          Welcome to <span className='app-name'>League Tools</span>
         </h1>
         <p>Start by connecting to your League Client</p>
       </div>
       <div>
         <button onClick={() => connect().then(() => navigate('../home'))}>Connect</button>
       </div>
+      <span className='version'>v2.0.0</span>
     </div>
   )
 }

b7bfbca4b5609fc927247caed4e0ac279305bf15
7rebux <imrebux@gmail.com>
2022-04-03 12:57:52 +0200

    :truck: Export theme stylesheet

--- component-lib/rollup.config.js
+++ component-lib/rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss';
+import copy from 'rollup-plugin-copy';
 
 const pack = require('./package.json');
 
@@ -26,5 +27,14 @@ export default {
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
     postcss(),
+    copy({
+      targets: [
+        {
+          src: 'src/theme.scss',
+          dest: 'build',
+          rename: 'theme.scss',
+        },
+      ],
+    }),
   ],
 };

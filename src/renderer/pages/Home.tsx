8cebea9362b35447cfa526f311651af50e1a9b0a
7rebux <imrebux@gmail.com>
2022-08-19 12:17:19 +0200

    :sparkles: Add home page / dashboard

---
+++ src/renderer/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Home.scss';
+
+const Home: React.FC = () => {
+  return (
+    <div className='home-page'>
+      <span>Home</span>
+    </div>
+  );
+};
+
+export default Home;

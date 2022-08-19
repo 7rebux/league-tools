20b8e80cb48e627fb595849ffb925f152ec29f62
7rebux <imrebux@gmail.com>
2022-08-19 12:16:40 +0200

    :sparkles: Add navbar

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,18 +1,63 @@
+import { Badge, SummonerIcon } from 'component-lib';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Connect from './pages/Connect';
-import Icons from './pages/Icons';
+import { Connect, Home, Icons } from './pages';
+
+function NavBar() {
+  return (
+    <div id='navbar'>
+      <div className='nav'>
+        <div className='nav-item selected'>
+          <img src='assets/home.png' />
+          <span>Home</span>
+        </div>
+        <div className='nav-item'>
+          <img src='assets/icon.png' />
+          <span>Icon</span>
+        </div>
+        <div className='nav-item'>
+          <img src='assets/background.png' />
+          <span>Background</span>
+        </div>
+        <div className='nav-item'>
+          <img src='assets/status.png' />
+          <span>Status</span>
+        </div>
+      </div>
+      <div className='profile'>
+        <div className='icon'>
+          <SummonerIcon
+            width={35}
+            height={35}
+            iconId={3333}
+            availability='chat'
+          />
+        </div>
+        <span>Summoner Name</span>
+        <Badge text='34231' icon='assets/be.png' backgroundColor='#5098DA' />
+        <Badge text='2109' icon='assets/rp.png' backgroundColor='#EA5D5F' />
+      </div>
+    </div>
+  );
+}
 
 function render() {
   ReactDOM.render(
-    <BrowserRouter>
-      <Routes>
-        <Route path='/main_window' element={<Icons />} />
-      </Routes>
-    </BrowserRouter>,
+    <div id='main'>
+      <NavBar />
+      <div id='content'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/connect' element={<Connect />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/icons' element={<Icons />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>,
     document.body
   );
 }
---
+++ src/renderer/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as Connect } from './Connect';
+export { default as Home } from './Home';
+export { default as Icons } from './Icons';

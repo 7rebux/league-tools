faac981ff9928c005291995b9dd8ece63a9a656a
7rebux <imrebux@gmail.com>
2022-10-10 12:40:48 +0200

    :lipstick: Add availability to profile

--- src/renderer/pages/Home/Home.tsx
+++ src/renderer/pages/Home/Home.tsx
@@ -4,6 +4,18 @@ import { useLcuData } from '../../components/LcuContext';
 import './Home.scss';
 
 const RANK_CREST_URL = 'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-static-assets/global/default/images/ranked-mini-crests/';
+const COLORS = new Map<string, string>([
+    ['UNRANKED',    '#404241'],
+    ['IRON',        '#6b6b64'],
+    ['BRONZE',      '#a46628'],
+    ['SILVER',      '#b5b5b5'],
+    ['GOLD',        '#d6a738'],
+    ['PLATINUM',    '#80aba4'],
+    ['DIAMOND',     '#71b0d1'],
+    ['MASTER',      '#7840a3'],
+    ['GRANDMASTER', '#9e3342'],
+    ['CHALLENGER',  '#1298e0'],
+]);
 
 const Home: React.FC = () => {
   const lcuData = useLcuData();
@@ -15,27 +27,14 @@ const Home: React.FC = () => {
     return `${rank.charAt(0) + rank.substring(1).toLowerCase()} ${division}`;
   };
 
-  const getRankColor = () => {
-    const colors = new Map<string, string>([
-        ['UNRANKED',    '#404241'],
-        ['IRON',        '#6b6b64'],
-        ['BRONZE',      '#a46628'],
-        ['SILVER',      '#b5b5b5'],
-        ['GOLD',        '#d6a738'],
-        ['PLATINUM',    '#80aba4'],
-        ['DIAMOND',     '#71b0d1'],
-        ['MASTER',      '#7840a3'],
-        ['GRANDMASTER', '#9e3342'],
-        ['CHALLENGER',  '#1298e0'],
-    ]);
-
-    return colors.get(lcuData.me.lol.rankedLeagueTier);
-  };
-
   return (
     <div className='home-page'>
       <div className='profile'>
-        <SummonerIcon iconId={lcuData.me.icon} size={90} />
+        <SummonerIcon 
+          iconId={lcuData.me.icon} 
+          availability={lcuData.me.availability}
+          size={90} 
+        />
         <div className='info'>
           <div>{lcuData.me.name} <span className='id'>#{lcuData.me.gameTag}</span></div>
           <div className='badges'>
@@ -46,7 +45,7 @@ const Home: React.FC = () => {
             <Badge 
               text={getRankText()} 
               icon={`${RANK_CREST_URL}${lcuData.me.lol.rankedLeagueTier.toLowerCase()}.png`}
-              backgroundColor={getRankColor()}
+              backgroundColor={COLORS.get(lcuData.me.lol.rankedLeagueTier)}
             />
           </div>
         </div>

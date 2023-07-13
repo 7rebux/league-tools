c69b53277e4b95d8c87d4aa4507d97cf24a13d76
7rebux <58339645+7rebux@users.noreply.github.com>
2023-07-13 22:09:21 +0200

    :heavy_minus_sign: Remove unnecessary storybook addons

--- component-lib/.storybook/main.js
+++ component-lib/.storybook/main.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
   stories: ['../src/**/*.stories.@(tsx|mdx)'],
-  addons: ['@storybook/addon-docs', '@storybook/addon-controls', '@storybook/addon-actions', 'storybook-dark-mode', '@storybook/addon-measure', '@storybook/addon-outline'],
+  addons: ['@storybook/addon-docs'],
   webpackFinal: async config => {
     config.module.rules.push({
       test: /\.(scss|sass)$/,
